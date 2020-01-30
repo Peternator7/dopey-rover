016222e3995eff2dc0219d23faacd28a227242a4
Peter Glotfelty 🚀 <peglotfe@microsoft.com>
2020-01-30 02:52:30 +0000

    Expand grammar

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,18 @@ use dopey_rover::parsing::parser;
 
 fn main() {
     let program = r#"MyFunction a b = {
-        a = 1.0;
-        try a;
-        a
-    };"#;
+    True = new {};
+    False = new {};
+    Some = new {};
+    None = new {};
+    (1 + 1) :: "a" :: []
+};"#;
 
     if let Ok((_, toks)) = lexer::tokenize(program) {
-        println!("{:?}", toks);
+        // println!("{:?}", toks);
         println!(
             "{:?}",
-            parser::statement::parse_statement(&*toks) //parser::pattern::parse_top_level_pattern(&*toks)
+            parser::statement::parse_statement(&*toks).unwrap().1 //parser::pattern::parse_top_level_pattern(&*toks)
         );
     }
 }
